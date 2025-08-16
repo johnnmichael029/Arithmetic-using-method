aead0189a01017ddaf65f1f57198b0e37d9abc87
John Michael De Torres <johnmichaeldetorres029@gmail.com>
2025-08-16 23:52:06 +0800

    Update Program.vb

--- Arithmetic using method/Program.vb
+++ Arithmetic using method/Program.vb
@@ -3,9 +3,7 @@ Imports System
 Module Program
     Dim num1, num2, user_sel As Integer
     Sub Main(args As String())
-
         CallMethods()
-
     End Sub
 
     Function AddNumbers(ByVal num1 As Integer, ByVal num2 As Integer, ByVal opt As Integer)
@@ -15,17 +13,14 @@ Module Program
     End Function
 
     Function UserInput()
-
         Console.Write("Enter first number: ")
         num1 = Console.ReadLine()
-
         Console.Write("Enter second number: ")
-        num2 = Console.ReadLine() 
+        num2 = Console.ReadLine()
         Return num1 & num2
     End Function
 
     Function UserSelection()
-
         Console.Write("[1] Add" & vbCrLf & "[2] Subtract" & vbCrLf & "[3] Multiply" & vbCrLf & "[4] Divide" & vbCrLf & "Select operation: ")
         user_sel = Console.ReadLine()
         Return user_sel
@@ -37,7 +32,6 @@ Module Program
         Dim again As String = Console.ReadLine()
         If again.ToUpper().Equals("Y") Then
             CallMethods()
-
         Else
             Console.WriteLine("Thank You!")
             Environment.Exit(0)
